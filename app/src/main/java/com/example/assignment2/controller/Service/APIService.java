6bdf8bef2ccf5716c0bf3d707a56c2b5c79f4218
phu0n9 <45571475+phu0n9@users.noreply.github.com>
2021-02-14 20:01:30 +0700

    Update APIService.java

--- app/src/main/java/com/example/assignment2/controller/Service/APIService.java
+++ app/src/main/java/com/example/assignment2/controller/Service/APIService.java
@@ -9,7 +9,7 @@ public interface APIService {
     @Headers(
             {
                     "Content-Type:application/json",
-                    "Authorization:key=AAAAb9LeXcQ:APA91bF6WKzE6ULNChTuzo1D_nYrEJs9T8mHvTRt6-efXuMCDzlwIFaIA8vNB2pyMLCf4tn39hwxjW4rB6d-G4j_0GJg_-dnCkYk3dHLAXvb6TLu5YNNFIgFXIijAS2X4njy5JupO0ar" // Your server key refer to video for finding your server key
+                    "Authorization:key=XXXXXXXXXXXXXX" // Your server key refer to video for finding your server key
             }
     )
 
